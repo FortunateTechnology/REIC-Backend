c2f3946d21f56fd061075fd64c80e4fd1b2efb12
manon618 <manon2029@gmail.com>
2024-01-15 16:01:43 +0700

    fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,10 +25,10 @@ class UserController extends Controller
      */
     function __construct()
     {
-        $this->middleware('permission:member-list|member-create|member-edit|member-delete', ['only' => ['index', 'show']]);
-        $this->middleware('permission:member-create', ['only' => ['create', 'store']]);
-        $this->middleware('permission:member-edit', ['only' => ['edit', 'update']]);
-        $this->middleware('permission:member-delete', ['only' => ['destroy']]);
+        $this->middleware('permission:user-list|user-create|user-edit|user-delete', ['only' => ['index', 'show']]);
+        $this->middleware('permission:user-create', ['only' => ['create', 'store']]);
+        $this->middleware('permission:user-edit', ['only' => ['edit', 'update']]);
+        $this->middleware('permission:user-delete', ['only' => ['destroy']]);
     }
 
     /**
